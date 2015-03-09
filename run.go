c1ab1d41f8c4d0fac3fee2d1f837ac018ba77d5f
Thomas Rooney <thomas.rooney11@imperial.ac.uk>
2015-03-09 12:24:56 +0000

    https login

--- run.go
+++ run.go
@@ -2,15 +2,37 @@ package main
 
 import "fmt"
 import "net/http"
+import "net/url"
 import "crypto/tls"
+import "gopkg.in/yaml.v2"
+import "io/ioutil"
+
+var configFileName = "config.yml"
+var secretFileName = "secret.yml"
+
+type Secret struct {
+	username string
+	password string
+}
 
 func main() {
+	secret := Secret{}
+	secretData, err := ioutil.ReadFile(secretFileName)
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.Unmarshal([]byte(secretData), &secret)
+	if err != nil {
+		panic(err)
+	}
+
 	tr := &http.Transport{
 		TLSClientConfig:    &tls.Config{InsecureSkipVerify: true},
 		DisableCompression: true,
 	}
 	client := &http.Client{Transport: tr}
-	resp, err := client.Get("https://example.com")
+	resp, err := client.PostForm("https://systmonline.tpp-uk.com/Login",
+		url.Values{"username": {secret.username}, "password": {secret.password}})
 	if err != nil {
 		fmt.Printf("Unexpected error[%+#v].\n", err)
 	}
