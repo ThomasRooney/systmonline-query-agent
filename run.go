acf8a67c2e25578916310b1dec65d9020e289e5a
Thomas Rooney <thomas.rooney11@imperial.ac.uk>
2015-03-09 12:02:06 +0000

    Secrecy, initial setup of tls

--- run.go
+++ run.go
@@ -1,7 +1,21 @@
 package main
 
 import "fmt"
+import "net/http"
+import "crypto/tls"
 
 func main() {
-	fmt.Printf("false\n")
+	tr := &http.Transport{
+		TLSClientConfig:    &tls.Config{InsecureSkipVerify: true},
+		DisableCompression: true,
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get("https://example.com")
+	if err != nil {
+		fmt.Printf("Unexpected error[%+#v].\n", err)
+	}
+	if resp.Status != "200 OK" {
+		fmt.Printf("Unexpected HTTP Response [%s]\n", resp.Status)
+	}
+	fmt.Printf("%+#v", resp)
 }
